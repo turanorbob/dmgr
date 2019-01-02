7fabddcd5512578c6c76f94101e486d216810b43
turanorbob <liaojian*2016>
2019-01-02 21:57:19 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="demo">
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+      data () {
+          return {
+              msg: 'Hello World!'
+          }
+      }
+  }
+</script>
+
+<style scope>
+* {
+    color: #FF0000;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  // 输入文件
+  entry: './src/main.js',
   output: {
-    filename: 'main.js',
+    // 文件名
+    filename: 'index.js',
+    // 静态目录,从这里取文件
+    publicPath:'/dist/',
+    // 输出目录
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  module: {
+        rules: [
+            //解析vue后缀文件
+            {
+              test: /\.vue$/,
+              loader: 'vue-loader',
+              exclude: /node_modules/
+            },
+            //用巴babel解析js文件 排除模块安装目录的文件
+            {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/
+            },
+            {
+                test: /\.css$/, 
+                use:['vue-style-loader','css-loader']
+            }
+        ]
+    }
 };
